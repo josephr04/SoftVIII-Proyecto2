1839e0df2c0487b84c9440e789e25920cf556f6e
josephr04 <jrosas.dev@gmail.com>
2025-10-31 21:47:40 -0500

    Feature: Registro de depositos

--- Proyecto2/deposito.Designer.vb
+++ Proyecto2/deposito.Designer.vb
@@ -26,25 +26,28 @@ Partial Class deposito
         Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Label1 = New Label()
         Label2 = New Label()
-        ComboBox1 = New ComboBox()
+        cbotiposdeposito = New ComboBox()
         Label3 = New Label()
-        DateTimePicker1 = New DateTimePicker()
         Label4 = New Label()
-        TextBox1 = New TextBox()
         btnregistrardeposito = New Button()
         Label5 = New Label()
-        DataGridView1 = New DataGridView()
-        Column1 = New DataGridViewTextBoxColumn()
-        Column2 = New DataGridViewTextBoxColumn()
-        Column3 = New DataGridViewTextBoxColumn()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        DataGridViewdep = New DataGridView()
+        fecha = New DataGridViewTextBoxColumn()
+        tipo = New DataGridViewTextBoxColumn()
+        monto = New DataGridViewTextBoxColumn()
+        txtmontodep = New ReaLTaiizor.Controls.AloneTextBox()
+        Panel1 = New Panel()
+        btncancelardep = New Button()
+        PoisonDateTimeDeposito = New ReaLTaiizor.Controls.PoisonDateTime()
+        CType(DataGridViewdep, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(52, 27)
+        Label1.Location = New Point(284, 68)
         Label1.Name = "Label1"
         Label1.Size = New Size(295, 31)
         Label1.TabIndex = 0
@@ -54,54 +57,41 @@ Partial Class deposito
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
-        Label2.Location = New Point(71, 89)
+        Label2.Location = New Point(303, 130)
         Label2.Name = "Label2"
         Label2.Size = New Size(169, 25)
         Label2.TabIndex = 1
         Label2.Text = "Tipo de Depósito:"
         ' 
-        ' ComboBox1
+        ' cbotiposdeposito
         ' 
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(71, 117)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(160, 28)
-        ComboBox1.TabIndex = 2
+        cbotiposdeposito.DropDownStyle = ComboBoxStyle.DropDownList
+        cbotiposdeposito.FormattingEnabled = True
+        cbotiposdeposito.Items.AddRange(New Object() {"Transferencia", "Depósito Directo"})
+        cbotiposdeposito.Location = New Point(303, 158)
+        cbotiposdeposito.Name = "cbotiposdeposito"
+        cbotiposdeposito.Size = New Size(200, 28)
+        cbotiposdeposito.TabIndex = 2
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
-        Label3.Location = New Point(71, 171)
+        Label3.Location = New Point(303, 212)
         Label3.Name = "Label3"
         Label3.Size = New Size(184, 25)
         Label3.TabIndex = 3
         Label3.Text = "Fecha del Depósito:"
         ' 
-        ' DateTimePicker1
-        ' 
-        DateTimePicker1.Location = New Point(71, 199)
-        DateTimePicker1.Name = "DateTimePicker1"
-        DateTimePicker1.Size = New Size(295, 27)
-        DateTimePicker1.TabIndex = 4
-        ' 
         ' Label4
         ' 
         Label4.AutoSize = True
         Label4.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
-        Label4.Location = New Point(71, 248)
+        Label4.Location = New Point(303, 289)
         Label4.Name = "Label4"
         Label4.Size = New Size(108, 25)
         Label4.TabIndex = 5
         Label4.Text = "Monto ($):"
-        ' 
-        ' TextBox1
-        ' 
-        TextBox1.BackColor = Color.FromArgb(CByte(179), CByte(196), CByte(214))
-        TextBox1.Location = New Point(71, 276)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(125, 27)
-        TextBox1.TabIndex = 6
         ' 
         ' btnregistrardeposito
         ' 
@@ -109,9 +99,9 @@ Partial Class deposito
         btnregistrardeposito.Cursor = Cursors.Hand
         btnregistrardeposito.FlatAppearance.BorderSize = 0
         btnregistrardeposito.FlatStyle = FlatStyle.Flat
-        btnregistrardeposito.Font = New Font("Segoe UI", 13F, FontStyle.Bold)
+        btnregistrardeposito.Font = New Font("Segoe UI", 12F)
         btnregistrardeposito.ForeColor = Color.White
-        btnregistrardeposito.Location = New Point(270, 276)
+        btnregistrardeposito.Location = New Point(408, 263)
         btnregistrardeposito.Name = "btnregistrardeposito"
         btnregistrardeposito.Size = New Size(117, 39)
         btnregistrardeposito.TabIndex = 8
@@ -122,15 +112,15 @@ Partial Class deposito
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
-        Label5.Location = New Point(685, 27)
+        Label5.Location = New Point(405, 422)
         Label5.Name = "Label5"
         Label5.Size = New Size(252, 31)
         Label5.TabIndex = 9
         Label5.Text = "Historial de Depósitos"
         ' 
-        ' DataGridView1
+        ' DataGridViewdep
         ' 
-        DataGridView1.BackgroundColor = Color.Teal
+        DataGridViewdep.BackgroundColor = Color.Teal
         DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
         DataGridViewCellStyle1.BackColor = SystemColors.Control
         DataGridViewCellStyle1.Font = New Font("Segoe UI", 11F)
@@ -138,37 +128,90 @@ Partial Class deposito
         DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
         DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
         DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
-        DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
-        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3})
-        DataGridView1.Location = New Point(606, 89)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.RowHeadersWidth = 34
-        DataGridView1.Size = New Size(416, 226)
-        DataGridView1.TabIndex = 10
+        DataGridViewdep.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        DataGridViewdep.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewdep.Columns.AddRange(New DataGridViewColumn() {fecha, tipo, monto})
+        DataGridViewdep.Location = New Point(320, 456)
+        DataGridViewdep.Name = "DataGridViewdep"
+        DataGridViewdep.RowHeadersWidth = 34
+        DataGridViewdep.Size = New Size(416, 226)
+        DataGridViewdep.TabIndex = 10
         ' 
-        ' Column1
+        ' fecha
         ' 
         DataGridViewCellStyle2.Font = New Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Column1.DefaultCellStyle = DataGridViewCellStyle2
-        Column1.HeaderText = "Fecha"
-        Column1.MinimumWidth = 6
-        Column1.Name = "Column1"
-        Column1.Width = 130
+        fecha.DefaultCellStyle = DataGridViewCellStyle2
+        fecha.HeaderText = "Fecha"
+        fecha.MinimumWidth = 6
+        fecha.Name = "fecha"
+        fecha.Width = 130
         ' 
-        ' Column2
+        ' tipo
         ' 
-        Column2.HeaderText = "Tipo"
-        Column2.MinimumWidth = 6
-        Column2.Name = "Column2"
-        Column2.Width = 125
+        tipo.HeaderText = "Tipo"
+        tipo.MinimumWidth = 6
+        tipo.Name = "tipo"
+        tipo.Width = 125
         ' 
-        ' Column3
+        ' monto
         ' 
-        Column3.HeaderText = "Monto"
-        Column3.MinimumWidth = 6
-        Column3.Name = "Column3"
-        Column3.Width = 125
+        monto.HeaderText = "Monto"
+        monto.MinimumWidth = 6
+        monto.Name = "monto"
+        monto.Width = 125
+        ' 
+        ' txtmontodep
+        ' 
+        txtmontodep.BackColor = Color.Transparent
+        txtmontodep.EnabledCalc = True
+        txtmontodep.Font = New Font("Segoe UI", 9F)
+        txtmontodep.ForeColor = Color.FromArgb(CByte(124), CByte(133), CByte(142))
+        txtmontodep.Location = New Point(303, 320)
+        txtmontodep.MaxLength = 32767
+        txtmontodep.MultiLine = False
+        txtmontodep.Name = "txtmontodep"
+        txtmontodep.ReadOnly = False
+        txtmontodep.Size = New Size(200, 36)
+        txtmontodep.TabIndex = 22
+        txtmontodep.TextAlign = HorizontalAlignment.Left
+        txtmontodep.UseSystemPasswordChar = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BorderStyle = BorderStyle.FixedSingle
+        Panel1.Controls.Add(btncancelardep)
+        Panel1.Controls.Add(PoisonDateTimeDeposito)
+        Panel1.Controls.Add(btnregistrardeposito)
+        Panel1.ForeColor = SystemColors.ControlText
+        Panel1.Location = New Point(266, 53)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(543, 328)
+        Panel1.TabIndex = 24
+        ' 
+        ' btncancelardep
+        ' 
+        btncancelardep.BackColor = SystemColors.ScrollBar
+        btncancelardep.Cursor = Cursors.Hand
+        btncancelardep.FlatAppearance.BorderSize = 0
+        btncancelardep.FlatStyle = FlatStyle.Flat
+        btncancelardep.Font = New Font("Segoe UI", 12F)
+        btncancelardep.ForeColor = SystemColors.ControlText
+        btncancelardep.Location = New Point(255, 263)
+        btncancelardep.Name = "btncancelardep"
+        btncancelardep.Size = New Size(138, 39)
+        btncancelardep.TabIndex = 26
+        btncancelardep.Text = "Cancelar"
+        btncancelardep.UseVisualStyleBackColor = False
+        ' 
+        ' PoisonDateTimeDeposito
+        ' 
+        PoisonDateTimeDeposito.CalendarFont = New Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        PoisonDateTimeDeposito.FontSize = ReaLTaiizor.Extension.Poison.PoisonDateTimeSize.Medium
+        PoisonDateTimeDeposito.Location = New Point(36, 186)
+        PoisonDateTimeDeposito.MinimumSize = New Size(0, 30)
+        PoisonDateTimeDeposito.Name = "PoisonDateTimeDeposito"
+        PoisonDateTimeDeposito.Size = New Size(200, 30)
+        PoisonDateTimeDeposito.TabIndex = 25
         ' 
         ' deposito
         ' 
@@ -176,36 +219,38 @@ Partial Class deposito
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImageLayout = ImageLayout.None
         ClientSize = New Size(1170, 689)
-        Controls.Add(DataGridView1)
+        Controls.Add(txtmontodep)
+        Controls.Add(DataGridViewdep)
         Controls.Add(Label5)
-        Controls.Add(btnregistrardeposito)
-        Controls.Add(TextBox1)
         Controls.Add(Label4)
-        Controls.Add(DateTimePicker1)
         Controls.Add(Label3)
-        Controls.Add(ComboBox1)
+        Controls.Add(cbotiposdeposito)
         Controls.Add(Label2)
         Controls.Add(Label1)
+        Controls.Add(Panel1)
         FormBorderStyle = FormBorderStyle.None
         Name = "deposito"
         Text = "deposito"
-        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridViewdep, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents cbotiposdeposito As ComboBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents btnregistrardeposito As Button
     Friend WithEvents Label5 As Label
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewdep As DataGridView
+    Friend WithEvents txtmontodep As ReaLTaiizor.Controls.AloneTextBox
+    Friend WithEvents fecha As DataGridViewTextBoxColumn
+    Friend WithEvents tipo As DataGridViewTextBoxColumn
+    Friend WithEvents monto As DataGridViewTextBoxColumn
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PoisonDateTimeDeposito As ReaLTaiizor.Controls.PoisonDateTime
+    Friend WithEvents btncancelardep As Button
 End Class
--- Proyecto2/deposito.vb
+++ Proyecto2/deposito.vb
@@ -1,3 +1,74 @@
-﻿Public Class deposito
+﻿Imports MySql.Data.MySqlClient
 
+Public Class deposito
+
+    Private Sub deposito_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler txtmontodep.TextChanged, AddressOf SoloDineroTextBox
+        CargarDepositos()
+    End Sub
+
+    Private Sub CargarDepositos()
+        Try
+            Using conn As MySqlConnection = ObtenerConexion()
+                conn.Open()
+
+                ' Traer todos los depósitos
+                Dim query As String = "SELECT tipo_deposito, fecha_deposito, monto FROM depositos ORDER BY fecha_deposito DESC"
+
+                Using cmd As New MySqlCommand(query, conn)
+                    Dim da As New MySqlDataAdapter(cmd)
+                    Dim dt As New DataTable
+                    da.Fill(dt)
+
+                    DataGridViewdep.AutoGenerateColumns = False
+                    DataGridViewdep.DataSource = dt
+
+                    DataGridViewdep.Columns("fecha").DataPropertyName = "fecha_deposito"
+                    DataGridViewdep.Columns("tipo").DataPropertyName = "tipo_deposito"
+                    DataGridViewdep.Columns("monto").DataPropertyName = "monto"
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar los depósitos: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnregistrardeposito_Click(sender As Object, e As EventArgs) Handles btnregistrardeposito.Click
+        If String.IsNullOrWhiteSpace(txtmontodep.Text) OrElse
+           cbotiposdeposito.SelectedValue Is Nothing Then
+            MessageBox.Show("Por favor complete todos los campos antes de registrar.", "Campos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Try
+            Using conn As MySqlConnection = ObtenerConexion()
+                conn.Open()
+                Dim query As String = "INSERT INTO depositos (tipo_deposito, fecha_deposito, monto)
+                                      VALUES (@tipo_deposito, @fecha_deposito, @monto)"
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@tipo_deposito", cbotiposdeposito.SelectedItem)
+                    cmd.Parameters.AddWithValue("@fecha_deposito", PoisonDateTimeDeposito.Value)
+                    cmd.Parameters.AddWithValue("@monto", Convert.ToDecimal(txtmontodep.Text))
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+            MessageBox.Show("Cheque registrado exitosamente.")
+
+            LimpiarCampos()
+            Me.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error al registrar el cheque: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub LimpiarCampos()
+        txtmontodep.Text = ""
+        txtmontodep.Text = ""
+        PoisonDateTimeDeposito.Value = DateTime.Now
+        cbotiposdeposito.SelectedIndex = -1
+    End Sub
+
+    Private Sub btncancelardep_Click(sender As Object, e As EventArgs) Handles btncancelardep.Click
+        LimpiarCampos()
+    End Sub
 End Class
